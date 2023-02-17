22f0bfb24fb7e184ef26c733da1a5ebceb8ec131
karishma gupta <karishma15102002@gmail.com>
2023-02-17 17:07:41 +0530

    added the square the nums array using map function

--- javascript/array.js
+++ javascript/array.js
@@ -22,7 +22,10 @@ for(let i=0; i<arr1.length;i++){
     console.log(arr1[i]);     // it will print all the elements of arr1 in console by for loop
 }
 
-//2. another way to iterate over array elements is using built-in function 
+
+
+//2. another way to iterate over array elements is using built-in forEach function
+//forEach function does not have any return type (it will not return anything)*************imp****************** 
 //array_Name.forEach(function_Name);     // here function need to declare seperately
 arr1.forEach(print);          // forEach function behaves like a for loop and function around paranthesis work like a code inside the for loop block
 function print(value){
@@ -37,35 +40,66 @@ arr1.forEach(function (value){
 } );
 
 
-//another way to write forEach function
+//another way to write forEach function##########################################################
 //syntax    array_Name.forEach((parameter) => {block of code});
 arr1.forEach((value) => {
     console.log(value);
 });
 
 
-// ARRAY PROPERTIES
-//1. length --> It is ARRAY PROPERTY that will tells the size of an array (means total no.of elements present in an array)
-console.log(arr1.length);
-console.log( typeof(arr1[6]), arr1[6]);   
+//3.Third way to iterate over array elements which is array_Name.map(function); in-built function
+// array_Name.map(function); has a return value which is an array (it returns the array)
+let mapArr = arr1.map(function (value){
+    return 10*value;
+})
+console.log(mapArr);
+
+//ex.  based on forEach function
+let square = [1,2,3,4,5];
+square.forEach(function (value,index){
+    square[index] = value*value;
+});
+//now square array changes to = [1,4,9,16,25]
+console.log(square);
+
+square.forEach((value,index) => {square[index] = value*value;});
+// sqaure = [ 1, 16, 81, 256, 625 ]
+console.log(square);
+
+//square the num array using map function
+let nums = [1,2,3,4];
+ let numsSquare = nums.map(function (value){
+    return value*value;
+})
+
+console.log(numsSquare);
 
 
-// ARRAY METHODS*************************************************
 
-// 1. push -- It is a method  that added the element to an array at the last.
-arr1.push(59393);                         // syntax -->       array_Name.push(data);
-console.log(arr1);
 
-//2. pop -- It is a method that is used to remove an element from an array at the last
-arr1.pop();                            //Syntax -->          array_Name.pop();
-console.log(arr1);
 
-//3. shift -- It is a method that is used to remove an element from an array at the start (it is similiar to the pop but the subtle difference is that it removed from the start)
-arr1.shift();                            //Syntax -->          array_Name.shift();
-console.log(arr1);
+// // ARRAY PROPERTIES
+// //1. length --> It is ARRAY PROPERTY that will tells the size of an array (means total no.of elements present in an array)
+// console.log(arr1.length);
+// console.log( typeof(arr1[6]), arr1[6]);   
 
-//4. unshift   --> It is a method  that added the element to an array at the start.(similiar to the push but it added at the start)
-arr1.unshift("unshift");                         // syntax -->       array_Name.unshift(data);
-console.log(arr1);
+
+// // ARRAY METHODS*************************************************
+
+// // 1. push -- It is a method  that added the element to an array at the last.
+// arr1.push(59393);                         // syntax -->       array_Name.push(data);
+// console.log(arr1);
+
+// //2. pop -- It is a method that is used to remove an element from an array at the last
+// arr1.pop();                            //Syntax -->          array_Name.pop();
+// console.log(arr1);
+
+// //3. shift -- It is a method that is used to remove an element from an array at the start (it is similiar to the pop but the subtle difference is that it removed from the start)
+// arr1.shift();                            //Syntax -->          array_Name.shift();
+// console.log(arr1);
+
+// //4. unshift   --> It is a method  that added the element to an array at the start.(similiar to the push but it added at the start)
+// arr1.unshift("unshift");                         // syntax -->       array_Name.unshift(data);
+// console.log(arr1);
 
 
