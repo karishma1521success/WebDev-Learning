5df090415034aa6aa4c1fdbb68d3a4f7d4a6e2e3
karishma gupta <karishma15102002@gmail.com>
2023-02-17 20:19:36 +0530

    added splice method and push and unshift could take multiple values to add to an array

--- javascript/array.js
+++ javascript/array.js
@@ -25,7 +25,7 @@ for(let i=0; i<arr1.length;i++){
 
 
 //2. another way to iterate over array elements is using built-in forEach function
-//forEach function does not have any return type (it will not return anything)*************imp****************** 
+//forEach function does not have any return type means void (it will not return anything)*************imp****************** 
 //array_Name.forEach(function_Name);     // here function need to declare seperately
 arr1.forEach(print);          // forEach function behaves like a for loop and function around paranthesis work like a code inside the for loop block
 function print(value){
@@ -48,10 +48,10 @@ arr1.forEach((value) => {
 
 
 //3.Third way to iterate over array elements which is array_Name.map(function); in-built function
-// array_Name.map(function); has a return value which is an array (it returns the array)
+// array_Name.map(function); has a return value which is an array (it returns the array)[array return type]
 let mapArr = arr1.map(function (value){
-    return 10*value;
-})
+    return 10*value;                          // if we don't return anything then it will return undefined 
+})                                            // means mapArr = [undefined, undefined, undefined,undefined];
 console.log(mapArr);
 
 //ex.  based on forEach function
@@ -66,7 +66,7 @@ square.forEach((value,index) => {square[index] = value*value;});
 // sqaure = [ 1, 16, 81, 256, 625 ]
 console.log(square);
 
-//square the num array using map function
+//square the num array using map function********************************************************************
 let nums = [1,2,3,4];
  let numsSquare = nums.map(function (value){
     return value*value;
@@ -90,6 +90,9 @@ console.log( typeof(arr1[6]), arr1[6]);
 arr1.push(59393);                         // syntax -->       array_Name.push(data);
 console.log(arr1);
 
+arr1.push(3,5,7,3.3);          
+console.log(arr1);             // can also push multiple elements to an array at the last.
+
 //2. pop -- It is a method that is used to remove an element from an array at the last
 arr1.pop();                            //Syntax -->          array_Name.pop();
 console.log(arr1);
@@ -101,5 +104,21 @@ console.log(arr1);
 //4. unshift   --> It is a method  that added the element to an array at the start.(similiar to the push but it added at the start)
 arr1.unshift("unshift");                         // syntax -->       array_Name.unshift(data);
 console.log(arr1);
+
+arr1.unshift(3,6,5);          // can add multiple values to an array at the start
+console.log(arr1);       
+
+
+//5. splice -- It is a method that delete an element at any index.
+// syntax --  array_Name.slice(index,deleteCount);
+let arr2 = [1,3,4,7,86,45,0,5.5];
+arr2.splice(2,1);    // It is telling to delete the element at index 2 and only the one element
+console.log(arr2);    // arr2 = [ 1, 3,7, 86, 45, 0, 5.5]
+
+arr2.splice(3,3);    // It tell to delete the 3 elements starting from index 3 including 3rd index element.
+console.log(arr2);
+
+arr2.splice(1);     // Here I have not given delete count so it will remove all the elements from index1 include
+console.log(arr2);
 
 
