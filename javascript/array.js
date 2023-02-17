7e43d1d9429c77787dc8ff36b61b4f29a71736e7
karishma gupta <karishma15102002@gmail.com>
2023-02-17 15:55:40 +0530

    added array loop iteration in array file

--- javascript/array.js
+++ javascript/array.js
@@ -1,4 +1,6 @@
 // array in js is a object which is used to store the muliple values together in a single variable.
+// You can have objects in an Array. You can have functions in an Array. You can have arrays in an Array:
+
 
 let arr = [];              // empty array ( array without any elements)
 console.log(arr);           // we can direct access all the elements of the array by using the variable name. but we can't do this in java
@@ -11,7 +13,27 @@ console.log(arr1[0]);                 // access elements of array (0-based index
 console.log(arr1[6]);                 
 
 arr1[3] = "bye"                       // changing the elements of an array
-console.log(arr1);     
+console.log(arr1);  
+
+
+//LOOPING ARRAY ELEMENTS
+//1. first way to iterate over array elements is for loop
+for(let i=0; i<arr1.length;i++){
+    console.log(arr1[i]);     // it will print all the elements of arr1 in console by for loop
+}
+
+//2. another way to iterate over array elements is using built-in function 
+//array_Name.forEach(function_Name);
+arr.forEach(print);          // forEach function behaves like a for loop and function around paranthesis work like a code inside the for loop block
+function print(value){
+    console.log(value);
+}
+
+
+// ARRAY PROPERTIES
+//1. length --> It is ARRAY PROPERTY that will tells the size of an array (means total no.of elements present in an array)
+console.log(arr1.length);
+console.log( typeof(arr1[6]), arr1[6]);   
 
 
 // ARRAY METHODS*************************************************
@@ -32,7 +54,4 @@ console.log(arr1);
 arr1.unshift("unshift");                         // syntax -->       array_Name.unshift(data);
 console.log(arr1);
 
-//5. length --> It is method that will tells the size of an array (means total no.of elements present in an array)
-console.log(arr1.length);
 
-console.log( typeof(arr1[6]), arr1[6]);
