8197ae08c4190037f00e533ac8dc1e47402a66c1
karishma gupta <karishma15102002@gmail.com>
2023-02-21 00:50:59 +0530

    applied onclick attribute and event listener('click',function)

---
+++ javascript/dom/domEvents.js
@@ -0,0 +1,12 @@
+//event
+
+//1. access the html element at which we want to add some event listener
+//2. .addEventListener(event_name, function_Name);  note - event_name around '' and only function name donot call
+function alertShow(){
+    alert("You have clicked on button just now");
+}
+document.getElementById('btn').addEventListener('click',alertShow);
+//another way  we can also add event listener on html element by giving attribute onclick = function to html tag
+function sayHello(){
+    alert("hello using the onclick attribute from say hello function");
+}
