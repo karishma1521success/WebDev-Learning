333c86ae8c12808eda271fe520f9f98411191daf
karishma gupta <karishma15102002@gmail.com>
2023-02-23 20:54:23 +0530

    moved imp.js into the folder

---
+++ javascript/impFunction/imp.js
@@ -0,0 +1,6 @@
+// here you found the imp functions which might you need
+
+let a = [1,2,0,-2,89,45];
+// function to sort the array  syntax-- array_Name(function (a,b){return a-b});
+a.sort(function(a,b){return a-b});
+console.log(a);
