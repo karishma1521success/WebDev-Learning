61f64a3bdb1091375177ff5d62181a0684351d14
karishma gupta <karishma15102002@gmail.com>
2023-02-24 12:00:43 +0530

    added setTimeout function

--- javascript/impFunction/imp.js
+++ javascript/impFunction/imp.js
@@ -1,6 +1,3 @@
 // here you found the imp functions which might you need
 
-let a = [1,2,0,-2,89,45];
-// function to sort the array  syntax-- array_Name(function (a,b){return a-b});
-a.sort(function(a,b){return a-b});
-console.log(a);
+//1. setTimeout (function)  -- It is a method to delay the execution of function.
