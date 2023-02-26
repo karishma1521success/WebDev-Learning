1ea9743d1e1697b9eddb4aa8e28542c1986e195d
karishma gupta <karishma15102002@gmail.com>
2023-02-26 19:36:43 +0530

    added the date() method

--- javascript/impFunction/imp.js
+++ javascript/impFunction/imp.js
@@ -1,3 +1,9 @@
 // here you found the imp functions which might you need
+//methods - date(),
 
 //1. setTimeout (function)  -- It is a method to delay the execution of function.
+
+//2. date();  *********************** 
+let date = Date();     // This date() function / method show the date with time in this format - Sun Feb 26 2023 19:31:12 GMT+0530 (India Standard Time)
+console.log(date);
+
