2d93945ca5cb99ca40ff528549ce73b283c3a587
karishma gupta <karishma15102002@gmail.com>
2023-03-17 15:01:26 +0530

    added execSync function which runs the vs code

---
+++ node/os.js
@@ -0,0 +1,18 @@
+let os = require('os');
+console.log(os);  // This will return the object and this method is used for getting the details about operating system
+
+console.log(os.hostname());  // This will return the hostname of an os (in my case it's returning karishma)
+
+console.log(os.arch());     // This will return the byte of os (in my case it's x64)
+
+console.log(os.networkInterfaces());    // This will return an object and having the key:value pair which tells about the network details
+
+console.log(os.userInfo());    // returns the user info object [in my case it's returning an object of
+/*{
+    uid: -1,
+    gid: -1,
+    username: 'karis',
+    homedir: 'C:\\Users\\karis',
+    shell: null
+  }]*/
+// if we use os.userInfo only then it will give whole function but did not run for running the function we have to call the function
---
+++ node/path.js
@@ -0,0 +1,8 @@
+"E:\Carrer\weblearning\node\path.js"
+let path = require('path');
+console.log(path);
+
+let directoryName = __dirname;
+console.log(directoryName);
+
+console.log(path.join(directoryName,"karishms.js"));
