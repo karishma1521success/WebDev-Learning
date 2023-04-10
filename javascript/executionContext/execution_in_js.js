d19498dd2b925e5a733c8c2afac9f5cb1c9546e3
karishma gupta <karishma15102002@gmail.com>
2023-04-10 15:42:12 +0530

    added important things to keep in mind while execution any javascript code

--- javascript/executionContext/execution_in_js.js
+++ javascript/executionContext/execution_in_js.js
@@ -1,2 +1,10 @@
 // Here we understand how javascript code runs 
 
+// JavaScript code runs from top to bottom and left to right
+// Note : Memory allocates first then execution happens 
+
+// Before Execution global Execution context add on call stack 
+// Execution context has two thing global and this and have two parts memory allocation and code execution
+// whenever function Executes a function add on call stack and created a another execution context and in that context first memory allocates and then function code executes
+
+
