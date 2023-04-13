0882c98086fe45d31bfbacbdb35e4d9e3b0b6dff
karishma gupta <karishma15102002@gmail.com>
2023-04-13 19:33:42 +0530

    updated

---
+++ node/webScrapping/requestResponse.js
@@ -0,0 +1,8 @@
+// web scraping - It's a automated task to extract the data from the websites.The program which extracts the data from websites is called a web scraper. You are going to learn to write web scrapers in JavaScript.
+/*
+There are mainly two parts to web scraping.
+
+Getting the data using request libraries and a headless browser.
+Parsing the data to extract the exact information that we want from the data.
+*/
+// Now to make a request node has a module name - request but it is not pre-installed in node like path, fs so firstly we have to install the module (request) by using npm(node package manager which works like a playstore in a) 
