f51902142faf62952671aa3ab6dc283508bd8c90
karishma gupta <karishma15102002@gmail.com>
2023-04-15 13:25:23 +0530

    added how js dom helps in the extraction of data from the html file we got

--- node/webScrapping/requestResponse.js
+++ node/webScrapping/requestResponse.js
@@ -9,6 +9,7 @@ Parsing the data to extract the exact information that we want from the data.
 //1. Now to make a request node has a module name - request but it is not pre-installed in node like path, fs so firstly we have to install the module (request) by using npm(node package manager which works like a playstore in android) [[ to install use command -->   npm install request     or  npm i request   ]]
 
 const request = require('request');
+/*
 console.log(request);
 
 request('http://www.google.com', function (error, response, body) {
@@ -19,6 +20,22 @@ request('http://www.google.com', function (error, response, body) {
   console.log('body:', body); // Print the HTML for the Google homepage.
    }
 });
+*/
 
 //here if else is saying if we get anyy error while scraping the data then give an error if not then show the data of that website ( that data we will get in the html form);
+//Above function is not a asynchornous means it's a synchronous (first data will fetch then remaining code will execute)
+// To make the above code asynchornous make a function a callback function
 
+const link = "https://www.bing.com/cricketdetails?q=CSK%20vs%20LSG%20cricket&IsCricketV3=1&QueryTimeZoneId=India%20Standard%20Time&ResponseType=FullScore&Intent=None&Provider=SI&Lang=English&ScenarioName=SingleGame&CricketTournamentId=5157&GameId=222174&";
+
+request(link , cb)
+function cb(error, response, body){
+   if(error){
+      console.error('error:', error); // Print the error if one occurred
+     }else{
+  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the link website webpage.
+    //now we want to extract the data from that Html file we got but that file is too large that to extract a particular data will be a very hectic task so to make this easy jsdom comes in picture to make the extract data task easy
+    
+     }
+}
