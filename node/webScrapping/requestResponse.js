35a3b107c51cbea0e9f8368f37c791e7abb305f9
karishma gupta <karishma15102002@gmail.com>
2023-04-21 20:00:51 +0530

    basic template of jsdom

--- node/webScrapping/requestResponse.js
+++ node/webScrapping/requestResponse.js
@@ -29,7 +29,7 @@ request('http://www.google.com', function (error, response, body) {
 //Above function is not a asynchornous means it's a synchronous (first data will fetch then remaining code will execute)
 // To make the above code asynchornous make a function a callback function
 
-const link = "https://www.bing.com/cricketdetails?q=CSK%20vs%20LSG%20cricket&IsCricketV3=1&QueryTimeZoneId=India%20Standard%20Time&ResponseType=FullScore&Intent=None&Provider=SI&Lang=English&ScenarioName=SingleGame&CricketTournamentId=5157&GameId=222174&";
+const link = "https://www.bing.com/cricketdetails?q=PBKS%20vs%20RCB%20cricket&IsCricketV3=1&QueryTimeZoneId=India%20Standard%20Time&ResponseType=FullScore&Intent=None&Provider=SI&Lang=English&ScenarioName=SingleGame&CricketTournamentId=5157&GameId=222199&";
 
 request(link , cb)
 function cb(error, response, html){
@@ -40,9 +40,10 @@ function cb(error, response, html){
     console.log('body:', html); // Print the HTML for the link website webpage.
     //now we want to extract the data from that Html file we got but that file is too large that to extract a particular data will be a very hectic task so to make this easy jsdom comes in picture to make the extract data task easy
     //how jsdom works - we send the selector to js dom and js dom on that basis of selector it searcch that element having those selectors and gives that element as response.
-    const dom = new JSDOM(body);
+    const dom = new JSDOM(html);
     const document = dom.window.document;  // This is a document means actual content (in window we also have tab and other search bar in the html we don't want that we just want actual doucment )
-
+    
+    
 
      }
 }
