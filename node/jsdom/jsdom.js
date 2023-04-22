d4688b80935dd5e3423655ed729989019d743092
karishma gupta <karishma15102002@gmail.com>
2023-04-22 15:42:15 +0530

    update

--- node/jsdom/jsdom.js
+++ node/jsdom/jsdom.js
@@ -4,3 +4,5 @@ const {JSDOM} = jsdom;  // this is for accesing all the keys of jsdom object mou
 const dom = new JSDOM(html);
  const document = dom.window.document;  // This is a document means actual content (in window we also have tab and other search bar in the html we don't want that we just want actual doucment )
  // basic code to use jsdom
+
+ //jsdom is used to get the data from the doucment.
